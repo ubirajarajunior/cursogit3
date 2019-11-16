3274cd24dadd1c6f042ac6d19473e4b2c2ec25f6
Ubirajara Junior <ununesjunior@gmail.com>
2019-11-16 20:29:58 -0300

    Projeto iniciado mostrando oi mundo

---
+++ Projeto_Teste/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Projeto_Teste {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Oi mundo!");
+            Console.ReadLine();
+        }
+    }
+}
