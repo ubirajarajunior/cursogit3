17316b7c44c95aff44f084f0bfd09d0ed3bdae04
Ubirajara Junior <ununesjunior@gmail.com>
2019-11-16 21:38:23 -0300

    Mensagem de bom dia hoje

--- Projeto_Teste/Program.cs
+++ Projeto_Teste/Program.cs
@@ -5,7 +5,8 @@ namespace Projeto_Teste {
         static void Main(string[] args) {
             Console.WriteLine("Oi mundo!");
             Console.WriteLine("Bom dia");
-            
+            Console.WriteLine("Como você está hoje ?");
+
         }
     }
 }
